c761dc2497513cb8d4dbc4558610d8e5ab7c2778
github username <github email address>
2018-06-28 18:23:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.insert(-2, "and ")
+    array[0..-2].join(", ") << array[-1]
+
+  end
 
 end
