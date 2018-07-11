e5332d99a713ab6ff3507ab83c3fde030c881e02
bwadlow <bwadlow@gmail.com>
2018-07-11 01:27:28 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.length==1
+  array.join()
+elsif array.length==2
+  array.join(" and ")
+else
+  last=array.pop()
+  str=array.join(', ')
+  str << ", and "
+  str << last
+end
 end
