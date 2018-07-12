a5648cd207844595eb9f6a66184c6df7dd1bfcc8
trisjones <trisjones04@gmail.com>
2018-07-12 16:04:47 -0400

    Done with the oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    new_last = "and " + array.last
+    array.map!{|i| i == array.last ? new_last : i}.join(", ")
+  end
 end
